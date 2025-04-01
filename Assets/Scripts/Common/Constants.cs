967455ecdc653d663248c3634bd96bc535ef5fc3
oriorori <junha3679@gmail.com>
2025-04-01 13:12:51 +0900

    [Feat] URL 추가

---
+++ Assets/Scripts/Common/Constants.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System;
+
+public class Constants
+{
+    #region Network
+    
+    public const string ServerURL = "http://localhost:3000";
+    public const string GameServerURL = "ws://localhost:3000";
+
+
+    #endregion
+    
+}
